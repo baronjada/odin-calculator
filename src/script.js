d5c7e8d4fd6f31979ed8d81a7dd366f9592ff8ca
Jada Baron <jadachantellebaron@gmail.com>
2025-06-11 17:58:51 +0100

    Build basic math operation functions and test

---
+++ src/script.js
@@ -0,0 +1,22 @@
+// Calc basic math operation functions
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+console.log("add function", add(2, 4));
+console.log("subtract function", subtract(10, 5));
+console.log("multiply function", multiply(10, 5));
+console.log("divide function", divide(10, 5));
