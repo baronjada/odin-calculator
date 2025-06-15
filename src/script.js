14fb139ddd5e6104bd4d3f5c2d33fd927b66b720
Jada Baron <jadachantellebaron@gmail.com>
2025-06-15 15:55:19 +0100

    Add styling to calc container

--- src/script.js
+++ src/script.js
@@ -1,18 +1,12 @@
-let calcFirstNum = 10;
-let calcOperator = "+";
-let calcSecondNum = 9;
+let calcFirstNum;
+let calcOperator;
+let calcSecondNum;
 
 function operate(num1, operator, num2) {
   return add(num1, num2);
 }
 
-console.log(
-  "operate function",
-  operate(calcFirstNum, calcOperator, calcSecondNum)
-);
-
 // Calc basic math operation functions
-
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -29,18 +23,29 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-console.log("add function", add(2, 4));
-console.log("subtract function", subtract(10, 5));
-console.log("multiply function", multiply(10, 5));
-console.log("divide function", divide(10, 5));
+// console.log("add function", add(2, 4));
+// console.log("subtract function", subtract(10, 5));
+// console.log("multiply function", multiply(10, 5));
+// console.log("divide function", divide(10, 5));
 
 let calcDisplay = document.querySelector(".calc-display");
 
 function updateCalcDisplay(event) {
-  calcDisplay.innerText = event.target.textContent;
+  calcFirstNum = event.target.textContent;
+  calcDisplay.innerText = calcFirstNum;
+  console.log("calc first num", calcFirstNum);
+
+  calcOperator = event.target.textContent;
+  calcDisplay.innerText = calcOperator;
+  console.log("calc operator", calcFirstNum);
+
+  calcSecondNum = event.target.textContent;
+  calcDisplay.innerText = calcSecondNum;
+  console.log("calc second num", calcFirstNum);
 }
 
 let calcButtons = document.querySelectorAll(".calc-buttons");
+
 calcButtons.forEach((button) => {
   button.addEventListener("click", updateCalcDisplay);
 });
